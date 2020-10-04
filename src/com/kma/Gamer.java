78e40b2d62957e4fdb1a08d66213ee4c40551125
Oleksii Lozovoi <o.lozovoi@ukma.edu.ua>
2020-10-04 22:05:57 +0300

    add Gamer.java class which will emulate player

---
+++ src/com/kma/Gamer.java
@@ -0,0 +1,29 @@
+package com.kma;
+
+import java.awt.*;
+
+// will emulate pressing keyboard buttons with supplied key codes list
+public class Gamer extends Thread {
+    private final int[] path;
+
+    public Gamer(int[] path) {
+        this.path = path;
+    }
+
+    @Override
+    public void run() {
+//        super.run();
+        try {
+            Robot robot = new Robot();
+            Thread.sleep(500);
+
+            for (int key : path) {
+                robot.keyPress(key);
+                robot.keyRelease(key);
+                Thread.sleep(100);
+            }
+        } catch (AWTException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
